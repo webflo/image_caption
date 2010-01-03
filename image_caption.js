02da3c5ca4df27c83abb69711375e30c31cf2784
davidwhthomas <davidwhthomas>
2010-01-03 05:39:18 +0000

    Update to address http://drupal.org/node/300779 thanks awolfey and ngmaloney

--- image_caption.js
+++ image_caption.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
     var imgwidth = $(this).width();
     var imgheight = $(this).height();
     var captiontext = $(this).attr('title');
+    var style = $(this).attr('style');
     var alignment = $(this).attr('align');
-    $(this).attr({align:""});
-    $(this).wrap("<div class=\"image-caption-container\" style=\"float:" + alignment + "\"></div>");
+    //Clear image styles to prevent conflicts with parent div
+    $(this).attr('style','');
+    $(this).wrap("<div class=\"image-caption-container\" style=\"" + style + "; text-align: " + alignment + "\"></div>");
     $(this).parent().width(imgwidth);
     $(this).parent().append("<div class=\"image-caption\">" + captiontext + "</div>");
   });
