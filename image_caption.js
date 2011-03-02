9aec17e68e3c974379296842475fa9095cfcc57b
webflo <florian@webflo.org>
2011-03-02 19:55:10 +0100

    port image_caption. apply from callison.
    - http://drupal.org/node/1008886#comment-4159278
    - http://drupal.org/files/issues/d7upgrade-1008886_0.patch

--- image_caption.js
+++ image_caption.js
@@ -1,19 +1,20 @@
-/*$Id$*/
-$(document).ready(function(){
-  $("img.caption").each(function(i) {
-    var imgwidth = $(this).width();
-    var imgheight = $(this).height();
-    var captiontext = $(this).attr('title');
-    var style = $(this).attr('style');
-    var alignment = $(this).attr('align');
-    //Clear image styles to prevent conflicts with parent div
-    $(this).attr({align:""});
-    $(this).attr({style:""});
-    $(this).wrap("<span class=\"image-caption-container\" style=\"display:block;" + style + "; float: " + alignment + "\"></span>");
-    $(this).parent().addClass('image-caption-container-' + alignment);
-    if(imgwidth != 'undefined' && imgwidth != 0){
-      $(this).parent().width(imgwidth);
-    }
-    $(this).parent().append("<span style=\"display:block;\" class=\"image-caption\">" + captiontext + "</span>");
+(function ($) {
+  $(document).ready(function(){
+    $("img.caption").each(function(i) {
+      var imgwidth = $(this).width();
+      var imgheight = $(this).height();
+      var captiontext = $(this).attr('title');
+      var style = $(this).attr('style');
+      var alignment = $(this).attr('align');
+      //Clear image styles to prevent conflicts with parent div
+      $(this).attr({align:""});
+      $(this).attr({style:""});
+      $(this).wrap("<span class=\"image-caption-container\" style=\"display:block;" + style + "; float: " + alignment + "\"></span>");
+      $(this).parent().addClass('image-caption-container-' + alignment);
+      if(imgwidth != 'undefined' && imgwidth != 0){
+        $(this).parent().width(imgwidth);
+      }
+      $(this).parent().append("<span style=\"display:block;\" class=\"image-caption\">" + captiontext + "</span>");
+    });
   });
-});
+})(jQuery);
